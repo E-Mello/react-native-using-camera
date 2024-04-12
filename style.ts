559b6802ed4e674811ecee5d61edefc2784c2e71
Édio Melo <e.mello.dev@gmail.com>
2024-04-12 09:06:04 -0400

    Finished the basic camera configuration with react-native + expo go + typescript

---
+++ style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  preview: {
+    alignSelf: "stretch",
+    flex: 1,
+  },
+});
+
+export default styles;
